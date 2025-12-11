f2ac527bf485f77990e56097b8d6574b0bcd09be
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2025-12-11 11:15:41 +0900

    refactor : 매직넘버 제거

--- Assets/2. Scripts/UI/UI_Crosshair.cs
+++ Assets/2. Scripts/UI/UI_Crosshair.cs
@@ -4,8 +4,18 @@ using DG.Tweening;
 
 public class UI_Crosshair : MonoBehaviour
 {
-    [Header("참조")] [SerializeField] private Gun _gun;
+    [Header("참조")]
+    [SerializeField] private Gun _gun;
+    
+    [Header("Fire Effect 설정")]
+    [Tooltip("격발 시 crosshair 스케일링")]
+    [SerializeField] private float _fireScaling = 1.3f;
+    
+    [Tooltip("crosshair 스케일링 기간")]
+    [SerializeField] private float _upScalingDuration = 0.05f;
+    [SerializeField] private float _downScalingDuration = 0.1f;
 
+    
     private RectTransform _rectTransform;
 
     private Sequence _scailSequence;
@@ -35,8 +45,8 @@ public class UI_Crosshair : MonoBehaviour
         _gun?.OnFire.AddListener(OnFireEffect);
 
         _scailSequence = DOTween.Sequence()
-            .Append(_rectTransform.DOScale(1.3f, 0.05f))
-            .Append(_rectTransform.DOScale(1f, 0.1f))
+            .Append(_rectTransform.DOScale(1f * _fireScaling, _upScalingDuration))
+            .Append(_rectTransform.DOScale(1f, _downScalingDuration))
             .SetAutoKill(false)
             .Pause();
     }
