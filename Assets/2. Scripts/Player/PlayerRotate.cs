76dfdbcf91465537b169c413af9ad369900b8154
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2025-12-19 13:56:58 +0900

    fix : 플레이어 몸체 회전 버그

--- Assets/2. Scripts/Player/PlayerRotate.cs
+++ Assets/2. Scripts/Player/PlayerRotate.cs
@@ -5,8 +5,10 @@ public class PlayerRotate : MonoBehaviour
 {
     [Header("회전 설정")]
     [SerializeField] private float _rotatationSpeed = 200f;
-    private float _accumulationX = 0f;
-    
+
+    [Header("카메라 참조")]
+    [SerializeField] private CameraRotate _cameraRotate;
+
     //참조
     private PlayerInput _input;
 
@@ -15,19 +17,23 @@ public class PlayerRotate : MonoBehaviour
         Init();
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        if (GameManager.Instance.State != EGameState.Playing
-            && GameManager.Instance.State != EGameState.Playing) return;
-        float mouseX = _input.MouseX;
+        if (GameManager.Instance.State != EGameState.Playing) return;
         
-        _accumulationX += mouseX * _rotatationSpeed * Time.deltaTime;
-        transform.eulerAngles = new Vector3(0, _accumulationX, 0);
+        if (_cameraRotate != null)
+        {
+            transform.eulerAngles = new Vector3(0, _cameraRotate.AccumulationX, 0);
+        }
     }
 
     private void Init()
     {
         _input = GetComponent<PlayerInput>();
-        
+
+        if (_cameraRotate == null)
+        {
+            _cameraRotate = Camera.main.GetComponent<CameraRotate>();
+        }
     }
 }
